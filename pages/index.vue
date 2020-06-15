a66d9f8d00979541420edde1b7824e4ff94d3873
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-06-15 11:49:29 +0100

    create navbar component and index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,59 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        vue-post
-      </h1>
-      <h2 class="subtitle">
-        My great Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <Navbar />
+    <div class="blogs-page">
+      <div class="main-content">
+        <div class="container">
+          <div class="columns is-mobile">
+            <div class="column is-8 is-offset-2">
+              <div class="section">
+                <div class="title">
+                  <h1>Newest Posts</h1>
+                  <hr />
+                </div>
+                <div class="post">
+                  <a class="post-header post-header-link clickable">
+                    <h4 class="title is-4">Some Title</h4>
+                    <h5 class="subtitle is-5">Some Subtitle</h5>
+                  </a>
+                  <div class="post-content">by Filip Jerga, 27th Jan, 2019</div>
+                </div>
+                <div class="post">
+                  <a class="post-header post-header-link clickable">
+                    <h4 class="title is-4">Some Title</h4>
+                    <h5 class="subtitle is-5">Some Subtitle</h5>
+                  </a>
+                  <div class="post-content">by Filip Jerga, 27th Jan, 2019</div>
+                </div>
+              </div>
+              <!-- end of post -->
+            </div>
+            <!-- end of side bar -->
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import Navbar from "~/components/Navbar";
 export default {
   components: {
-    Logo
+    Navbar
   }
-}
+};
 </script>
-
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+.post {
+  margin-bottom: 20px;
+  padding: 5px;
+  border-bottom: 2px solid transparent;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.post-content {
+  font-style: italic;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.post:hover {
+  border-bottom: 2px solid #e8e8e8;
 }
 </style>
